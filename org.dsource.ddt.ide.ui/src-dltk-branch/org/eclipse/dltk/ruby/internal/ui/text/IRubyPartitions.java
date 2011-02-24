72812ff73ce3249e6755361871df66c9ec5b5b31
Bruno Medeiros <devnull@localhost>
2011-02-24 00:27:42 +0000

    cleanup of some RubyDLTK code

--- org.dsource.ddt.ide.ui/src-dltk-branch/org/eclipse/dltk/ruby/internal/ui/text/IRubyPartitions.java
+++ org.dsource.ddt.ide.ui/src-dltk-branch/org/eclipse/dltk/ruby/internal/ui/text/IRubyPartitions.java
@@ -1,20 +1,10 @@
 package org.eclipse.dltk.ruby.internal.ui.text;
 
-import org.eclipse.dltk.ruby.core.RubyConstants;
-import org.eclipse.jface.text.IDocument;
+import mmrnmhrm.ui.text.DeePartitions;
 
+// adapter
 public interface IRubyPartitions {
-	public static final String RUBY_PARTITIONING = RubyConstants.RUBY_PARTITIONING;
+	
+	public static final String RUBY_PARTITIONING = DeePartitions.DEE_PARTITIONING;
 
-//	public static final String RUBY_COMMENT = "__ruby_comment"; //$NON-NLS-1$
-//	public static final String RUBY_STRING = "__ruby_string"; //$NON-NLS-1$
-//	public static final String RUBY_SINGLE_QUOTE_STRING = "__ruby_single_quote_string"; //$NON-NLS-1$
-//	public static final String RUBY_PERCENT_STRING = "__ruby_percent_string"; //$NON-NLS-1$
-//	public static final String RUBY_DOC = "__ruby_doc"; //$NON-NLS-1$
-//
-//	public final static String[] RUBY_PARTITION_TYPES = new String[] {
-//			IDocument.DEFAULT_CONTENT_TYPE, IRubyPartitions.RUBY_DOC,
-//			IRubyPartitions.RUBY_COMMENT, IRubyPartitions.RUBY_STRING,
-//			IRubyPartitions.RUBY_SINGLE_QUOTE_STRING,
-//			IRubyPartitions.RUBY_PERCENT_STRING };
 }
