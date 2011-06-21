432b5ec17056fa9efe6eaf31cb4da22584d462c8
Bruno Medeiros <devnull@localhost>
2011-06-21 20:16:51 +0100

    cleanup

--- org.dsource.ddt.ide.core/src/org/dsource/ddt/ide/core/model/DeeModelElementUtil.java
+++ org.dsource.ddt.ide.core/src/org/dsource/ddt/ide/core/model/DeeModelElementUtil.java
@@ -1,28 +1,14 @@
 package org.dsource.ddt.ide.core.model;
 
 import static melnorme.utilbox.core.Assert.AssertNamespace.assertFail;
-import mmrnmhrm.core.DeeCore;
 
 import org.eclipse.dltk.ast.Modifiers;
 import org.eclipse.dltk.core.IMember;
 import org.eclipse.dltk.core.IModelElement;
-import org.eclipse.dltk.core.ModelException;
 
 import dtool.ast.definitions.EArcheType;
 
 public class DeeModelElementUtil {
-	
-	public static EArcheType elementFlagsToArcheType(IMember member) {
-		int flags;
-		try {
-			flags = member.getFlags();
-		} catch (ModelException e) {
-			// TODO throw instead?
-			DeeCore.log(e);
-			flags = 0; // Ignore, use empty flags
-		}
-		return elementFlagsToArcheType(member, flags);
-	}
 	
 	public static EArcheType elementFlagsToArcheType(IMember member, int flags) {
 		switch (member.getElementType()) {
