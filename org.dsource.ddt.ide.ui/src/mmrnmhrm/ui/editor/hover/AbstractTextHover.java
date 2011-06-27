f1e083727e8b644df83d47894e6420ccba964aa0
bruno.do.medeiros@gmail.com <bruno.do.medeiros@gmail.com>
2011-06-27 17:17:41 +0100

    removed dead code

--- org.dsource.ddt.ide.ui/src/mmrnmhrm/ui/editor/hover/AbstractTextHover.java
+++ org.dsource.ddt.ide.ui/src/mmrnmhrm/ui/editor/hover/AbstractTextHover.java
@@ -2,14 +2,8 @@ package mmrnmhrm.ui.editor.hover;
 
 import mmrnmhrm.ui.DeePlugin;
 
-import org.dsource.ddt.lang.ui.WorkbenchUtils;
-import org.eclipse.jface.text.AbstractReusableInformationControlCreator;
-import org.eclipse.jface.text.DefaultInformationControl;
-import org.eclipse.jface.text.IInformationControl;
 import org.eclipse.jface.text.IInformationControlCreator;
 import org.eclipse.jface.text.ITextHoverExtension;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.texteditor.ITextEditor;
 
 @SuppressWarnings("restriction")
@@ -50,34 +44,7 @@ implements ITextHoverExtension {
 	
 	@Override
 	public IInformationControlCreator getHoverControlCreator() {
-		if(true)
 		return super.getHoverControlCreator();
-		
-		// TODO: remove this?
-		if(false)
-		return new IInformationControlCreator() {
-			@Override
-			public IInformationControl createInformationControl(Shell parent) {
-				return new DefaultInformationControl(parent, "TODO",  
-						//new	org.eclipse.jface.internal.text.html.HTMLTextPresenter(true),
-						new org.eclipse.dltk.internal.ui.text.HTMLTextPresenter(true)
-						);
-			}
-		};
-		
-		Shell shell= WorkbenchUtils.getActiveWorkbenchShell();
-		if (shell == null
-				|| !org.eclipse.dltk.internal.ui.BrowserInformationControl
-						.isAvailable(shell))
-			return null;
-		
-		return new AbstractReusableInformationControlCreator() {
-			@Override
-			public IInformationControl doCreateInformationControl(Shell parent) {
-				return new org.eclipse.dltk.internal.ui.BrowserInformationControl(
-						parent, SWT.NO_TRIM, SWT.NONE, "STATUS");
-			}
-		};
 	}
 
 
