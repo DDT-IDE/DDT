ffd1e9ceaad6822d7767928501338e9b0a62beab
bruno.do.medeiros@gmail.com <bruno.do.medeiros@gmail.com>
2011-06-28 19:35:49 +0100

    Updated DeeDocumentationProvider

--- org.dsource.ddt.ide.ui/src/mmrnmhrm/ui/editor/doc/DeeDocumentationProvider.java
+++ org.dsource.ddt.ide.ui/src/mmrnmhrm/ui/editor/doc/DeeDocumentationProvider.java
@@ -1,7 +1,8 @@
 package mmrnmhrm.ui.editor.doc;
 
+import static melnorme.utilbox.core.Assert.AssertNamespace.assertFail;
+
 import java.io.Reader;
-import java.io.StringReader;
 
 import mmrnmhrm.ui.editor.hover.DeeDocTextHover;
 
@@ -11,7 +12,10 @@ import org.eclipse.dltk.core.IMember;
 import org.eclipse.dltk.core.ISourceModule;
 import org.eclipse.dltk.core.ISourceRange;
 import org.eclipse.dltk.core.ModelException;
+import org.eclipse.dltk.ui.documentation.IDocumentationResponse;
 import org.eclipse.dltk.ui.documentation.IScriptDocumentationProvider;
+import org.eclipse.dltk.ui.documentation.IScriptDocumentationProviderExtension2;
+import org.eclipse.dltk.ui.documentation.TextDocumentationResponse;
 
 import dtool.ast.ASTNeoNode;
 import dtool.ast.ASTNodeFinder;
@@ -20,7 +24,35 @@ import dtool.ast.ASTNodeFinder;
  * It is preferable that this documentation provider is not used as it has a more limited API.
  * Preferable to use {@link DeeDocTextHover} whenever possible.
  */
-public class DeeDocumentationProvider implements IScriptDocumentationProvider {
+public class DeeDocumentationProvider implements IScriptDocumentationProvider, IScriptDocumentationProviderExtension2 {
+	
+	@Deprecated
+	@Override
+	public Reader getInfo(IMember element, boolean lookIntoParents, boolean lookIntoExternal) {
+		return null;
+	}
+	
+	@Deprecated
+	@Override
+	public Reader getInfo(String content) {
+		// BM: used for keywords and some other unclear DLTK scenarios
+		// BM: note: but maybe not anymore on DLTK 3.0 IScriptDocumentationProviderExtension2
+		return null; 
+	}
+	
+	@Override
+	public IDocumentationResponse getDocumentationFor(Object element) {
+		if(element instanceof IMember) {
+			String header = getHeaderComment((IMember) element);
+			return header == null ? null : new TextDocumentationResponse(element, null, convertToHTML(header)); 
+		}
+		return null;
+	}
+	
+	protected String convertToHTML(String header) {
+		assertFail();
+		return header;
+	}
 	
 	protected String getHeaderComment(IMember member) {
 		ISourceRange range;
@@ -43,22 +75,6 @@ public class DeeDocumentationProvider implements IScriptDocumentationProvider {
 		ASTNeoNode pickedNode = ASTNodeFinder.findNeoElement(deeModule.neoModule, start, false);
 		
 		return DeeDocTextHover.getDocInfoForNode(pickedNode);
-	}
-	
-	@Override
-	public Reader getInfo(IMember member, boolean lookIntoParents, boolean lookIntoExternal) {
-		String header = getHeaderComment(member);
-		return (header == null) ? null : new StringReader(convertToHTML(header));
-	}
-	
-	protected String convertToHTML(String header) {
-		return header;
-	}
-	
-	@Override
-	public Reader getInfo(String content) {
-		// BM: used for keywords and some other unclear DLTK scenarios
-		return null; 
 	}
 	
 }
