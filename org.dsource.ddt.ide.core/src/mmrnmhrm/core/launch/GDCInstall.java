62b4a1cafba85d85c4ab8d8b7dcdd734688d3652
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2011-11-01 18:38:03 +0000

    cleanup

--- org.dsource.ddt.ide.core/src/mmrnmhrm/core/launch/GDCInstall.java
+++ org.dsource.ddt.ide.core/src/mmrnmhrm/core/launch/GDCInstall.java
@@ -1,22 +1,11 @@
 package mmrnmhrm.core.launch;
 
 
-import org.eclipse.core.runtime.IPath;
 
-public class GDCInstall extends CommonDeeInstall {
+public class GDCInstall extends CommonDeeInstall { 
 	
 	public GDCInstall(GDCInstallType type, String id) {
 		super(type, id);
-	}
-	
-	/** Get the path of the compiler directory */
-	public IPath getCompilerBasePath() {
-		return getInstallLocation().getPath().removeLastSegments(1);
-	}
-	
-	/** Get the full path of the compiler, including the executable */
-	public IPath getCompilerFullPath() {
-		return getInstallLocation().getPath();
 	}
 	
 	@Override
