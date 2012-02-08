c8cedcd61f56eed13e3d33c7bd64b906cbd8b7ec
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2012-02-08 15:15:55 +0000

    Fix for DLTK bug in ScriptElementLabels

--- org.dsource.ddt.ide.ui/src/mmrnmhrm/ui/DeeScriptElementLabels.java
+++ org.dsource.ddt.ide.ui/src/mmrnmhrm/ui/DeeScriptElementLabels.java
@@ -1,8 +1,59 @@
 package mmrnmhrm.ui;
 
+import org.eclipse.dltk.core.INamespace;
+import org.eclipse.dltk.core.IScriptFolder;
+import org.eclipse.dltk.core.IType;
+import org.eclipse.dltk.core.ModelException;
 import org.eclipse.dltk.ui.ScriptElementLabels;
 
 public class DeeScriptElementLabels extends ScriptElementLabels {
-
+	
+	@Override
+	protected void appendTypeQualification(IType type, long flags, StringBuffer buf) {
+		// -------------- Original DLTK 3.0 code:
+//		try {
+//			final INamespace namespace = type.getNamespace();
+//			if (namespace != null) {
+//				// TODO customize separator
+//				buf.append(namespace.getQualifiedName("."));
+//				buf.append(".");
+//				return;
+//			}
+//		} catch (ModelException e) {
+//			// ignore
+//			return;
+//		}
+//		IResource resource = type.getResource();
+//		IProjectFragment pack = null;
+//		if (resource != null) {
+//			IScriptProject project = type.getScriptProject();
+//			pack = project.getProjectFragment(resource);
+//		} else {
+//			pack = findProjectFragment(type);
+//		}
+//		if (pack == null) {
+//			pack = findProjectFragment(type);
+//		}
+//		getScriptFolderLabel(pack, (flags & QUALIFIER_FLAGS), buf);
+		
+		// -------------- 
+		try {
+			final INamespace namespace = type.getNamespace();
+			if (namespace != null) {
+				buf.append(namespace.getQualifiedName("."));
+				if(!namespace.isRoot()) {
+					buf.append(".");
+				}
+				return;
+			}
+		} catch (ModelException e) {
+			// ignore
+			return;
+		}
+		IScriptFolder pkg = type.getScriptFolder();
+		
+		getScriptFolderLabel(pkg, (flags & QUALIFIER_FLAGS), buf);
+		buf.append(".");
+	}
 	
 }
