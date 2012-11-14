8205258d1165f44c56b218cd1e6768cc35e83568
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2012-11-14 16:45:13 +0000

    Moved methods up

--- org.dsource.ddt.dtool/src-test/dtool/parser/Parser__FileParseTest.java
+++ org.dsource.ddt.dtool/src-test/dtool/parser/Parser__FileParseTest.java
@@ -3,20 +3,9 @@ package dtool.parser;
 import static melnorme.utilbox.core.Assert.AssertNamespace.assertTrue;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-
-import melnorme.utilbox.core.Function;
-import melnorme.utilbox.core.VoidFunction;
-import melnorme.utilbox.misc.ArrayUtil;
 
 import org.junit.Test;
-
-import dtool.DeeNamingRules_Test;
-import dtool.tests.MiscFileUtils;
 
 
 public abstract class Parser__FileParseTest extends Parser__CommonTest {
@@ -46,56 +35,6 @@ public abstract class Parser__FileParseTest extends Parser__CommonTest {
 	protected static void parseFile(File file, boolean failOnSyntaxErrors, boolean checkSourceRanges) {
 		String source = readStringFromFileUnchecked(file);
 		testParse(source, failOnSyntaxErrors ? false : null, checkSourceRanges);
-	}
-	
-	protected static ArrayList<File> getDeeModuleList(File folder, boolean recurseDirs) throws IOException {
-		return getDeeModuleList(folder, recurseDirs, false);
-	}
-	protected static ArrayList<File> getDeeModuleList(File folder, boolean recurseDirs, final boolean validCUsOnly)
-			throws IOException {
-		
-		final boolean addInAnyFileName = !validCUsOnly;
-		final ArrayList<File> fileList = new ArrayList<File>();
-		
-		VoidFunction<File> fileVisitor = new VoidFunction<File>() {
-			@Override
-			public Void evaluate(File file) {
-				if(file.isFile()) {
-					fileList.add(file);
-				}
-				return null;
-			}
-		};
-		
-		FilenameFilter filter = new FilenameFilter() {
-			@Override
-			public boolean accept(File parent, String childName) {
-				System.out.println("dir:" + parent +" " + childName);
-				File childFile = new File(parent, childName);
-				if(childFile.isDirectory()) {
-					// exclude team private folder, like .svn, and other crap
-					return !childName.startsWith(".");
-				} else {
-					return addInAnyFileName || DeeNamingRules_Test.isValidCompilationUnitName(childName);
-				}
-			}
-		};
-		MiscFileUtils.traverseFiles(folder, recurseDirs, fileVisitor, filter);
-		return fileList;
-	}
-	
-	public static Collection<Object[]> getTestFilesFromFolderAsParameterList(File folder) throws IOException {
-		assertTrue(folder.exists() && folder.isDirectory());
-		ArrayList<File> deeModuleList = getDeeModuleList(folder, true);
-		
-		Function<Object, Object[]> arrayWrap = new Function<Object, Object[]>() {
-			@Override
-			public Object[] evaluate(Object obj) {
-				return new Object[] { obj };
-			};
-		};
-		
-		return Arrays.asList(ArrayUtil.map(deeModuleList, arrayWrap, Object[].class));
 	}
 	
 }
--- org.dsource.ddt.dtool/src-test/dtool/tests/DToolBaseTest.java
+++ org.dsource.ddt.dtool/src-test/dtool/tests/DToolBaseTest.java
@@ -10,11 +10,22 @@
  *******************************************************************************/
 package dtool.tests;
 
+import static melnorme.utilbox.core.Assert.AssertNamespace.assertTrue;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
+import dtool.DeeNamingRules_Test;
+
+import melnorme.utilbox.core.Function;
+import melnorme.utilbox.core.VoidFunction;
+import melnorme.utilbox.misc.ArrayUtil;
 import melnorme.utilbox.misc.FileUtil;
 import melnorme.utilbox.misc.StringUtil;
 
@@ -41,6 +52,57 @@ public class DToolBaseTest extends CommonTestUtils {
 		} catch (IOException e) {
 			throw melnorme.utilbox.core.ExceptionAdapter.unchecked(e);
 		}
+	}
+	
+	/* -------------  Module list stuff   ------------ */
+	
+	protected static ArrayList<File> getDeeModuleList(File folder, boolean recurseDirs) throws IOException {
+		return getDeeModuleList(folder, recurseDirs, false);
+	}
+	protected static ArrayList<File> getDeeModuleList(File folder, boolean recurseDirs, final boolean validCUsOnly)
+			throws IOException {
+		assertTrue(folder.exists() && folder.isDirectory());
+		
+		final boolean addInAnyFileName = !validCUsOnly;
+		final ArrayList<File> fileList = new ArrayList<File>();
+		
+		VoidFunction<File> fileVisitor = new VoidFunction<File>() {
+			@Override
+			public Void evaluate(File file) {
+				if(file.isFile()) {
+					fileList.add(file);
+				}
+				return null;
+			}
+		};
+		
+		FilenameFilter filter = new FilenameFilter() {
+			@Override
+			public boolean accept(File parent, String childName) {
+				File childFile = new File(parent, childName);
+				if(childFile.isDirectory()) {
+					// exclude team private folder, like .svn, and other crap
+					return !childName.startsWith(".");
+				} else {
+					return addInAnyFileName || DeeNamingRules_Test.isValidCompilationUnitName(childName);
+				}
+			}
+		};
+		MiscFileUtils.traverseFiles(folder, recurseDirs, fileVisitor, filter);
+		return fileList;
+	}
+	
+	public static Collection<Object[]> getTestFilesFromFolderAsParameterList(File folder) throws IOException {
+		ArrayList<File> deeModuleList = getDeeModuleList(folder, true);
+		
+		Function<File, Object[]> arrayWrap = new Function<File, Object[]>() {
+			@Override
+			public Object[] evaluate(File obj) {
+				return new Object[] { obj };
+			};
+		};
+		
+		return Arrays.asList(ArrayUtil.map(deeModuleList, arrayWrap, Object[].class));
 	}
 	
 }
