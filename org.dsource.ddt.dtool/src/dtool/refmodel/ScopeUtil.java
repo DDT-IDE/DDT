add456a9f7a8355d86356fad41a2d3a467fa713c
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2013-05-02 21:26:30 +0100

    Fixed resolving bug with class definition super types.

--- org.dsource.ddt.dtool/src/dtool/refmodel/ScopeUtil.java
+++ org.dsource.ddt.dtool/src/dtool/refmodel/ScopeUtil.java
@@ -1,16 +1,18 @@
 package dtool.refmodel;
 
+import dtool.ast.DeclList;
 import dtool.ast.IASTNeoNode;
 import dtool.ast.definitions.DefinitionClass;
 
+// TODO: rewrite this
 public class ScopeUtil {
 
 	/** Finds the first outer scope of the given element 
 	 * (navigating through the element's parents). */
 	public static IScopeNode getOuterScope(IASTNeoNode elem) {
-		return getScopeNode(elem.getParent());
+		return getScopeNode(getNextParent(elem));
 	}
-
+	
 	/** Finds the first IScopeNode in the given elem chain of parents, 
 	 * including elem itself. This corresponds to the innermost lexical
 	 * scope available from elem. */
@@ -20,17 +22,22 @@ public class ScopeUtil {
 			if (elem instanceof IScopeNode)
 				return (IScopeNode) elem;
 			
-			/*BUG here should be:*/
-			//if (elem.getParent() instanceof DefinitionAggregate) {
-			if (elem.getParent() instanceof DefinitionClass) {
-				// Need to skip aggregate defunit scope 
-				elem = elem.getParent().getParent();
-				continue;
-			}
-			
-			elem = elem.getParent();
+			elem = getNextParent(elem);
 		}
 		return null;
+	}
+
+	public static IASTNeoNode getNextParent(IASTNeoNode elem) {
+		/*BUG here should be:*/
+		//if (elem.getParent() instanceof DefinitionAggregate) {
+		if (elem.getParent() instanceof DefinitionClass && !(elem instanceof DeclList)) {
+			// Need to skip aggregate defunit scope 
+			elem = elem.getParent().getParent();
+			return elem;
+		}
+		
+		elem = elem.getParent();
+		return elem;
 	}
 	
 }
