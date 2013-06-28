b0bd4f6a08818205d888bbdeb14b3e1fec699d21
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2013-06-28 21:58:17 +0100

    cleanup

--- org.dsource.ddt.dtool/src-util/melnorme/utilbox/misc/IteratorUtil.java
+++ org.dsource.ddt.dtool/src-util/melnorme/utilbox/misc/IteratorUtil.java
@@ -15,21 +15,24 @@ import java.util.Collections;
 import java.util.Iterator;
 
 public class IteratorUtil { 
-
+	
 	public static final Iterator<?> EMPTY_ITERATOR = Collections.EMPTY_LIST.iterator();
 	
 	@SuppressWarnings("unchecked")
 	public static <T> Iterator<T> getEMPTY_ITERATOR() {
 		return (Iterator<T>) EMPTY_ITERATOR;
 	}
-
+	
+	/** Recasts the type parameter of given iterator to a more specific type.
+	 * Safe to do if the returned iterator is used in a read only way with regards to the underlying collection.
+	 * @return the recasted iterator. */
 	@SuppressWarnings("unchecked")
-	public static <T> Iterator<T> recast(Iterator<? extends T> iter) {
-		return ((Iterator<T>) iter);
+	public static <T> Iterator<T> recast(Iterator<? extends T> iterator) {
+		return ((Iterator<T>) iterator);
 	}
-
+	
 	public static <T> Iterator<T> singletonIterator(T elem) {
 		return Collections.singletonList(elem).iterator();
 	}
-
+	
 }
