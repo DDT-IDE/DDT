57474a9137f9a65ff1aca4bb70da8a592ddedac5
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2013-07-26 19:10:49 +0100

    Removed some obsolote tests.

--- org.dsource.ddt.ide.core.tests/src-test/dtool/tests/ref/cc/CodeCompletion_DuplicatesTest.java
+++ org.dsource.ddt.ide.core.tests/src-test/dtool/tests/ref/cc/CodeCompletion_DuplicatesTest.java
@@ -17,38 +17,12 @@ public class CodeCompletion_DuplicatesTest extends CodeCompletion__Common {
 	
 	@Test
 	public void test1() throws Exception {
-		int offset = getMarkerStartOffset("/+@CC1+/");
-		testComputeProposals(offset, 1, false,
-				
-				"func(int aaa, List!(Foo) aaa)",
-				"func(int bbb, List!(Foo) bbb)",
-				"func(char a, List!(Foo) a)",
-				"func(int a, List!(Bar) a)",
-				"func()",
-				
-				
-				"foo_t", "fooalias" 
-		);
+
 	}
 	
 	@Test
 	public void test2() throws Exception {
-		int offset = getMarkerStartOffset("/+@CC2+/");
-		testComputeProposals(offset, 1, false,
-				"foolocalinner", "foolocal1", "fParam", "foobarvar",
-				
-				"func(int a, List!(Foo) a)",
-				"func(int bbb, List!(Foo) bbb)",
-				"func(char a, List!(Foo) a)",
-				"func(int a, List!(Bar) a)",
-				"func()",
-				
-				"func(int aaa, List!(Foo) aaa)", //TODO: should be removed due to overload sets
-				
-				"foovar", "foox", 
-				
-				"foo_t", "fooalias" 
-		);
+
 	}
 	
 }
