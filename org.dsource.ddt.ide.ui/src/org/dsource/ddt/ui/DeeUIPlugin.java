123e1c52d30e8aa1f90bfc84232b3675085f2403
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2013-11-13 16:49:29 +0000

    Workaround for lazy init of debug plugin contributions.

--- org.dsource.ddt.ide.ui/src/org/dsource/ddt/ui/DeeUIPlugin.java
+++ org.dsource.ddt.ide.ui/src/org/dsource/ddt/ui/DeeUIPlugin.java
@@ -13,7 +13,9 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.dltk.ui.text.ScriptTextTools;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
 
 import dtool.Logg;
 
@@ -22,7 +24,10 @@ public class DeeUIPlugin extends LangPlugin {
 	
 	public static final String PLUGIN_ID = "org.dsource.ddt.ide.ui";
 	// The convention for the id prefix for extensions contributed by this plugin
-	public static final String EXTENSIONS_IDPREFIX = PLUGIN_ID + "."; 
+	public static final String EXTENSIONS_IDPREFIX = PLUGIN_ID + ".";
+	
+	// ID to start the debug plugin automatically, if present
+	private static final String DEBUG_PLUGIN_ID = "org.dsource.ddt.ide.debug";
 	
 	public static boolean initialized; 
 	protected static DeeUIPlugin pluginInstance;
@@ -69,11 +74,27 @@ public class DeeUIPlugin extends LangPlugin {
 		Logg.main.println("Instance Location: " + Platform.getInstanceLocation().getURL());
 		
 		SWTUtilExt.enableDebugColorHelpers = Platform.inDebugMode();
+		
+		startDebugPlugin();
 	}
 	
 	public static void initializeAfterLoad(IProgressMonitor monitor) throws CoreException {
 		// nothing to do
 		monitor.done();
+	}
+	
+	private static void startDebugPlugin() {
+		// Force start of debug plugin, if present, so that UI contributions will be fully active.
+		// ATM, some UI contributions that dynamically manipulate enablement and state don't work correctly
+		// unless underlying plugin is started.
+		try {
+			Bundle debugPlugin = Platform.getBundle(DEBUG_PLUGIN_ID);
+			if(debugPlugin != null) {
+				debugPlugin.start(Bundle.START_TRANSIENT);
+			}
+		} catch (BundleException e) {
+			log(e);
+		}
 	}
 	
 	public ScriptTextTools getTextTools() {
