761b34d4abe82feceb7ff6ec48c0895d3107cf70
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2014-01-20 18:51:40 +0000

    Refactoring, removed unused code.

--- org.dsource.ddt.ide.debug/src/org/dsource/ddt/debug/core/DeeDebugLaunchConfigurationDelegate.java
+++ org.dsource.ddt.ide.debug/src/org/dsource/ddt/debug/core/DeeDebugLaunchConfigurationDelegate.java
@@ -15,11 +15,6 @@ import mmrnmhrm.core.launch.AbstractScriptLaunchConfigurationDelegateExtension;
 
 import org.dsource.ddt.ide.core.DeeNature;
 import org.eclipse.cdt.debug.core.ICDTLaunchConfigurationConstants;
-import org.eclipse.cdt.dsf.debug.sourcelookup.DsfSourceLookupDirector;
-import org.eclipse.cdt.dsf.debug.sourcelookup.DsfSourceLookupParticipant;
-import org.eclipse.cdt.dsf.gdb.launching.GdbLaunchDelegate;
-import org.eclipse.cdt.dsf.gdb.launching.LaunchUtils;
-import org.eclipse.cdt.dsf.service.DsfSession;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
@@ -29,12 +24,8 @@ import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.core.ILaunchManager;
-import org.eclipse.debug.core.model.ISourceLocator;
-import org.eclipse.debug.core.sourcelookup.ISourceLookupParticipant;
 import org.eclipse.dltk.launching.InterpreterConfig;
 import org.eclipse.dltk.launching.ScriptLaunchConfigurationConstants;
-
-import dtool.DToolBundle;
 
 public class DeeDebugLaunchConfigurationDelegate extends AbstractScriptLaunchConfigurationDelegateExtension {
 	
@@ -94,48 +85,6 @@ public class DeeDebugLaunchConfigurationDelegate extends AbstractScriptLaunchCon
 			IProgressMonitor monitor) throws CoreException {
 		String mode = launch.getLaunchMode();
 		gdbLaunchDelegate.launch(configuration, mode, launch, monitor);
-	}
-	
-	public static class GdbLaunchDelegateExtension extends GdbLaunchDelegate {
-		
-		@Override
-		public ILaunch getLaunch(ILaunchConfiguration configuration, String mode) throws CoreException {
-			setDefaultProcessFactory(configuration); // Reset process factory to what GdbLaunch expected
-			
-			ILaunch launch = super.getLaunch(configuration, mode);
-			// workaround for DLTK bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=419273
-			launch.setAttribute("org.eclipse.dltk.debug.debugConsole", "false");
-			return launch;
-		}
-		
-		@Override
-		protected ISourceLocator getSourceLocator(ILaunchConfiguration configuration, DsfSession session)
-				throws CoreException {
-			return super.getSourceLocator(configuration, session);
-		}
-		
-		@Override
-		protected IPath checkBinaryDetails(ILaunchConfiguration config) throws CoreException {
-			// Now verify we know the program to debug.
-			IPath exePath = LaunchUtils.verifyProgramPath(config, null);
-			// Finally, make sure the program is a proper binary.
-			
-			// BM: this code is disabled because without a project the binary verifier defaults to ELF on any platform
-			if(DToolBundle.UNIMPLEMENTED_FUNCTIONALITY) {
-				LaunchUtils.verifyBinary(config, exePath);
-			}
-			return exePath;
-		}
-		
-		@Override
-		protected DsfSourceLookupDirector createDsfSourceLocator(ILaunchConfiguration configuration, DsfSession session)
-				throws CoreException {
-			DsfSourceLookupDirector sourceLookupDirector = new DeeSourceLookupDirector(session);
-			
-			sourceLookupDirector.addParticipants( new ISourceLookupParticipant[]{ new DsfSourceLookupParticipant(session) } );
-			return sourceLookupDirector;
-		}
-		
 	}
 	
 }
