2f645322b34b616934d9b67d27bb3180ea183fb3
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2014-07-11 16:29:17 +0100

    Simplified source coloring preview file.

--- plugin_ide.ui/src/mmrnmhrm/ui/preferences/SourceColoringPreviewFile.d
+++ plugin_ide.ui/src/mmrnmhrm/ui/preferences/SourceColoringPreviewFile.d
@@ -1,36 +1,22 @@
-#!/usr/bin/dmd -run
 module coloring_preview;
-
 import std.stdio;
-
-/*
- * A multiline comment.
- */
- 
-int a = 1234; // Single line comment
-char b = 'z'; /// Single line doc comment
-
-/+ 
-void oldfunc() {
-	/+ A nested comment +/
-	int a = 2; // Another nested comment
-}
-+/
 
 /** A documentation comment.
  * TODO: put more D Doc operators here here 
  */
 public class Foo {
 	
-	private int mData;
-	
-    @property int data() { return mData; }	// read property
-	
 	@safe
 	pure bool func(immutable int[] anArray) nothrow {
-		if(anArray is null) return false;
+		int a = 1234; // Single line comment
+		char b = 'z'; /// Single line doc comment
+		string str = "a string";
+		
+		if(anArray is null) 
+			return false;
 		return (anArray.length == mData);
 	}
+	
 }
 
 string str = example("normal \"string\" ", x"0123456789ABCDEF", "utf8"c, "utf16"w, "utf32"d);
