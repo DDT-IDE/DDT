795b2bfa68d5ca8d2e9600200b84fb9b15c612bf
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2014-07-29 22:19:44 +0100

    Add sentinel file creation.

--- plugin_tooling/src/dtool/genie/cmdline/StartServerOperation.java
+++ plugin_tooling/src/dtool/genie/cmdline/StartServerOperation.java
@@ -10,8 +10,13 @@
  *******************************************************************************/
 package dtool.genie.cmdline;
 
+import static melnorme.utilbox.core.Assert.AssertNamespace.assertUnreachable;
+
+import java.io.File;
 import java.io.IOException;
 
+import melnorme.utilbox.misc.FileUtil;
+import melnorme.utilbox.misc.StringUtil;
 import dtool.engine.DToolServer;
 import dtool.genie.GenieMain.AbstractCmdlineOperation;
 import dtool.genie.GenieServer;
@@ -23,30 +28,51 @@ public class StartServerOperation extends AbstractCmdlineOperation {
 	}
 	
 	@Override
-	protected void handle() {
-		int portNumber = 0;
-		
-//		File file = new File(System.getProperty("user.home"), ".dtoolgenie");
-//		try {
-//			boolean created = file.createNewFile();
-//			if(created == false) {
-//				// ER
-//			}
-//		} catch (IOException e) {
-//			throw melnorme.utilbox.core.ExceptionAdapter.unchecked(e);
-//		}
-		
-		DToolServer dtoolServer = new DToolServer();
-		try {
-			new GenieServer(dtoolServer, portNumber).runServer();
-		} catch (IOException ioe) {
-			dtoolServer.logError("Error trying to listen for connection on port " + portNumber + ".", ioe);
-		}
-	}
-	
-	@Override
 	public String getOneLineSummary() {
 		return "Start the DToolGenie server.";
+	}
+	
+	protected DToolServer dtoolServer;
+
+	@Override
+	protected void handle() {
+		int requestedPortNumber = 0;
+		boolean force = true;
+		
+		dtoolServer = new DToolServer();
+		
+		final File sentinelFile = new File(System.getProperty("user.home"), ".dtoolgenie");
+		try {
+			boolean created = sentinelFile.createNewFile();
+			if(created == false && force == false) {
+				String message = "Failed to create server sentinel file, perhaps server is running already?";
+				errorBail(message + "  use argument 'force' to start anyways", null); // TODO
+			}
+		} catch (IOException e) {
+			errorBail("Error creating sentinel file " + sentinelFile, e);
+		}
+		sentinelFile.deleteOnExit();
+		
+		GenieServer genieServer;
+		try {
+			genieServer = new GenieServer(dtoolServer, requestedPortNumber);
+		} catch (IOException ioe) {
+			throw errorBail("Error trying to listen for connection on port " + requestedPortNumber + ".", ioe);
+		}
+		
+		try {
+			FileUtil.writeStringToFile(sentinelFile, ""+genieServer.getServerPortNumber(), StringUtil.UTF8);
+		} catch (IOException e) {
+			errorBail("Error writing to sentinel file " + sentinelFile, e);
+		}
+		
+		genieServer.runServer();
+	}
+	
+	protected RuntimeException errorBail(String message, Throwable throwable) {
+		dtoolServer.logError(message, throwable);
+		System.exit(1);
+		throw assertUnreachable();
 	}
 	
 }
