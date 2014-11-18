31388809635fe03fa320367e0fb6b3b785fdcd92
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2014-11-18 17:44:53 +0000

    Fix ScriptEditor cast error.

--- plugin_ide.ui/src/mmrnmhrm/ui/editor/DeeSourceViewerConfiguration.java
+++ plugin_ide.ui/src/mmrnmhrm/ui/editor/DeeSourceViewerConfiguration.java
@@ -29,16 +29,13 @@ import mmrnmhrm.ui.text.DeePartitions;
 
 import org.dsource.ddt.lang.ui.editor.AbstractLangSourceViewerConfiguration;
 import org.eclipse.cdt.ui.text.IColorManager;
-import org.eclipse.dltk.internal.ui.editor.EditorUtility;
 import org.eclipse.dltk.internal.ui.editor.ModelElementHyperlinkDetector;
-import org.eclipse.dltk.internal.ui.editor.ScriptSourceViewer;
 import org.eclipse.dltk.internal.ui.text.hover.ScriptInformationProvider_Mod;
 import org.eclipse.dltk.internal.ui.typehierarchy.HierarchyInformationControl;
 import org.eclipse.dltk.ui.text.ScriptPresentationReconciler;
 import org.eclipse.dltk.ui.text.completion.ContentAssistPreference;
 import org.eclipse.dltk.ui.text.hover.IScriptEditorTextHover;
 import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.jface.text.AbstractInformationControlManager;
 import org.eclipse.jface.text.DefaultInformationControl;
 import org.eclipse.jface.text.IAutoEditStrategy;
 import org.eclipse.jface.text.IDocument;
@@ -52,10 +49,15 @@ import org.eclipse.jface.text.hyperlink.IHyperlinkDetector;
 import org.eclipse.jface.text.information.IInformationPresenter;
 import org.eclipse.jface.text.information.IInformationProvider;
 import org.eclipse.jface.text.information.InformationPresenter;
+import org.eclipse.jface.text.quickassist.IQuickAssistAssistant;
+import org.eclipse.jface.text.quickassist.QuickAssistAssistant;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.internal.editors.text.EditorsPlugin;
 import org.eclipse.ui.texteditor.ITextEditor;
+import org.eclipse.ui.texteditor.spelling.SpellingCorrectionProcessor;
+import org.eclipse.ui.texteditor.spelling.SpellingService;
 
 public class DeeSourceViewerConfiguration extends AbstractLangSourceViewerConfiguration {
 	
@@ -197,6 +199,31 @@ public class DeeSourceViewerConfiguration extends AbstractLangSourceViewerConfig
 	@Override
 	public IContentAssistant getContentAssistant(ISourceViewer sourceViewer) {
 		return super.getContentAssistant(sourceViewer);
+	}
+	
+	/* -----------------  ----------------- */
+	
+	@Override
+	public IQuickAssistAssistant getQuickAssistAssistant(ISourceViewer sourceViewer) {
+		// TODO: use super methods in TextSourceViewerConfiguration
+		if (fPreferenceStore == null || !fPreferenceStore.getBoolean(SpellingService.PREFERENCE_SPELLING_ENABLED))
+			return null;
+
+		QuickAssistAssistant assistant= new QuickAssistAssistant();
+		assistant.setQuickAssistProcessor(new SpellingCorrectionProcessor());
+		assistant.setRestoreCompletionProposalSize(EditorsPlugin.getDefault().getDialogSettingsSection("quick_assist_proposal_size")); //$NON-NLS-1$
+		assistant.setInformationControlCreator(getQuickAssistAssistantInformationControlCreator());
+		
+		return assistant;
+	}
+	
+	private IInformationControlCreator getQuickAssistAssistantInformationControlCreator() {
+		return new IInformationControlCreator() {
+			@Override
+			public IInformationControl createInformationControl(Shell parent) {
+				return new DefaultInformationControl(parent, EditorsPlugin.getAdditionalInfoAffordanceString());
+			}
+		};
 	}
 	
 	// ================
