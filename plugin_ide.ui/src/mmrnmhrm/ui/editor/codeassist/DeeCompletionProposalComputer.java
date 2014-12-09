a5556385867c89625668f576d520edc39a2a7922
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2014-12-09 17:50:22 +0000

    added FIXME for possible bug

--- plugin_ide.ui/src/mmrnmhrm/ui/editor/codeassist/DeeCompletionProposalComputer.java
+++ plugin_ide.ui/src/mmrnmhrm/ui/editor/codeassist/DeeCompletionProposalComputer.java
@@ -1,8 +1,12 @@
 package mmrnmhrm.ui.editor.codeassist;
 
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.dltk.ui.text.completion.ScriptCompletionProposalCollector;
 import org.eclipse.dltk.ui.text.completion.ScriptCompletionProposalComputer;
 import org.eclipse.dltk.ui.text.completion.ScriptContentAssistInvocationContext;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
 import org.eclipse.jface.text.templates.TemplateCompletionProcessor;
 
 public class DeeCompletionProposalComputer extends ScriptCompletionProposalComputer {
@@ -18,6 +22,13 @@ public class DeeCompletionProposalComputer extends ScriptCompletionProposalCompu
 	@Override
 	protected TemplateCompletionProcessor createTemplateProposalComputer(ScriptContentAssistInvocationContext context) {
 		return new DeeTemplateCompletionProcessor(context);
+	}
+	
+	@Override
+	protected List<ICompletionProposal> computeScriptCompletionProposals(int offset,
+			ScriptContentAssistInvocationContext context, IProgressMonitor monitor) {
+		// FIXME need to update this to fix tests bug.
+		return super.computeScriptCompletionProposals(offset, context, monitor);
 	}
 	
 }
