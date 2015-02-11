2b3e4e059e81b092e4d8be2001adbe71f1005405
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2015-02-11 17:23:39 +0000

    Misc cleanup and perspective LANG updates.

--- plugin_ide.ui/src/mmrnmhrm/ui/wizards/DeeProjectWizard.java
+++ plugin_ide.ui/src/mmrnmhrm/ui/wizards/DeeProjectWizard.java
@@ -15,16 +15,12 @@ import static melnorme.utilbox.misc.MiscUtil.getClassResourceAsString;
 
 import java.lang.reflect.InvocationTargetException;
 
-import melnorme.lang.ide.core.utils.ResourceUtils;
 import melnorme.lang.ide.ui.dialogs.LangNewProjectWizard;
 import melnorme.lang.ide.ui.dialogs.LangProjectWizardFirstPage;
 import melnorme.lang.ide.ui.dialogs.WizardMessages;
 import mmrnmhrm.core.DeeCore;
-import mmrnmhrm.ui.DeeUIPlugin;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.operation.IRunnableWithProgress;
@@ -36,8 +32,6 @@ import dtool.dub.BundlePath;
  * D New Project Wizard.
  */
 public class DeeProjectWizard extends LangNewProjectWizard {
-	
-	public static final String WIZARD_ID = DeeUIPlugin.PLUGIN_ID + ".wizards.deeProjectWizard";
 	
 	protected static final String HelloWorld_DubJsonTemplate = getClassResourceAsString(
 		DeeProjectWizard.class, "hello_world.dub.json");
@@ -80,32 +74,16 @@ public class DeeProjectWizard extends LangNewProjectWizard {
 		
 		@Override
 		protected void configureCreatedProject(IProgressMonitor monitor) throws CoreException {
-			IProject project = getProject();
-			final IFile dubManifest = project.getFile(BundlePath.DUB_MANIFEST_FILENAME);
-			if(!dubManifest.exists()) {
-				
-				final IFolder folder = project.getFolder("source");
-				ResourceUtils.createFolder(folder, true, true, null);
-				
-				final IFile appFile = folder.getFile("app.d");
-				ResourceUtils.writeStringToFile(appFile, HelloWorld_ModuleContents);
-				
-				ResourceUtils.writeStringToFile(dubManifest, getDefaultDubJSon());
-				
-				revertActions.add(new IRevertAction() {
-					@Override
-					public void run(IProgressMonitor monitor) throws CoreException {
-						dubManifest.delete(false, monitor);
-						appFile.delete(false, monitor);
-						if(folder.members().length == 0) {
-							folder.delete(false, monitor);
-						}
-					}
-				});
-			}
+			createSampleHelloWorldBundle(BundlePath.DUB_MANIFEST_FILENAME, "source", "app.d");
 		}
 		
-		protected String getDefaultDubJSon() {
+		@Override
+		protected String getHelloWorldContents() {
+			return HelloWorld_ModuleContents;
+		}
+		
+		@Override
+		protected String getDefaultManifestFileContents() {
 			return HelloWorld_DubJsonTemplate.replace("%BUNDLE_NAME%", getProject().getName());
 		}
 		
