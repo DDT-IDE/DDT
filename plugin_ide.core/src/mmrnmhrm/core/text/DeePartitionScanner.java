cade2f78ea696106bfeb8d326651af7c0469611b
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2015-04-09 19:48:16 +0100

    LANG: Added character scanner, refactor partition scanner.
    Conflicts:
    	plugin_tooling/META-INF/MANIFEST.MF

--- plugin_ide.core/src/mmrnmhrm/core/text/DeePartitionScanner.java
+++ plugin_ide.core/src/mmrnmhrm/core/text/DeePartitionScanner.java
@@ -1,54 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2015 Bruno Medeiros and other Contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Bruno Medeiros - initial API and implementation
+ *******************************************************************************/
 package mmrnmhrm.core.text;
 
-import melnorme.lang.ide.core.text.PatternRule_Fixed;
+import melnorme.lang.ide.core.text.RuleBasedPartitionScannerExt;
 import melnorme.utilbox.collections.ArrayList2;
 
 import org.eclipse.jface.text.rules.IPredicateRule;
-import org.eclipse.jface.text.rules.IToken;
-import org.eclipse.jface.text.rules.RuleBasedPartitionScanner;
+import org.eclipse.jface.text.rules.PatternRule;
 import org.eclipse.jface.text.rules.Token;
 
-// See: http://www.digitalmars.com/d/2.0/lex.html
-public class DeePartitionScanner extends RuleBasedPartitionScanner {
-	// BM Note: we should be careful with having different rules return the same token
-	// Such behavior is not properly supported by RuleBasedPartitionScanner
+public class DeePartitionScanner extends RuleBasedPartitionScannerExt implements DeePartitions {
 	
-	private static final char NO_ESCAPE_CHAR = (char) -1;
-	
-	/**Creates the partitioner and sets up the appropriate rules. */
-	public DeePartitionScanner() {
-		IToken tkString = new Token(DeePartitions.DEE_STRING);
-		IToken tkRawString = new Token(DeePartitions.DEE_RAW_STRING);
-		IToken tkRawString2 = new Token(DeePartitions.DEE_RAW_STRING2);
-		IToken tkDelimString = new Token(DeePartitions.DEE_DELIM_STRING);
-		IToken tkCharacter = new Token(DeePartitions.DEE_CHARACTER);
-		IToken tkSingleComment = new Token(DeePartitions.DEE_SINGLE_COMMENT);
-		IToken tkSingleDocComment = new Token(DeePartitions.DEE_SINGLE_DOCCOMMENT);
-		IToken tkMultiComment = new Token(DeePartitions.DEE_MULTI_COMMENT);
-		IToken tkMultiDocComment = new Token(DeePartitions.DEE_MULTI_DOCCOMMENT);
-		IToken tkNestedComment = new Token(DeePartitions.DEE_NESTED_COMMENT);
-		IToken tkNestedDocComment = new Token(DeePartitions.DEE_NESTED_DOCCOMMENT);
+	// See: http://www.digitalmars.com/d/2.0/lex.html
+	@Override
+	protected void addRules(ArrayList2<IPredicateRule> rules) {
 		
-		ArrayList2<IPredicateRule> rules = new ArrayList2<>();
-		
-		rules.add(new PatternRule_Fixed("`", "`", tkRawString, NO_ESCAPE_CHAR, false, true));
-		rules.add(new PatternRule_Fixed("r\"", "\"", tkRawString2, NO_ESCAPE_CHAR, false, true));
+		rules.add(new PatternRule("`", "`", new Token(DEE_RAW_STRING), NO_ESCAPE_CHAR, false, true));
+		rules.add(new PatternRule("r\"", "\"", new Token(DEE_RAW_STRING2), NO_ESCAPE_CHAR, false, true));
 		// TODO: this rule is not accurate, need to use something like HereDocEnabledPartitioner to make it work
-		rules.add(new PatternRule_Fixed("q\"", "\"", tkDelimString, NO_ESCAPE_CHAR, false, true)); 
-		rules.add(new PatternRule_Fixed("\"", "\"", tkString, '\\', false, true));
-		rules.add(new PatternRule_Fixed("'", "'", tkCharacter, '\\', true, true));
+		rules.add(new PatternRule("q\"", "\"", new Token(DEE_DELIM_STRING), NO_ESCAPE_CHAR, false, true)); 
+		rules.add(new PatternRule("\"", "\"", new Token(DEE_STRING), '\\', false, true));
+		rules.add(new PatternRule("'", "'", new Token(DEE_CHARACTER), '\\', true, true));
 		
+		addStandardRules(rules, 
+			DeePartitions.DEE_SINGLE_COMMENT, 
+			DeePartitions.DEE_MULTI_COMMENT, 
+			DeePartitions.DEE_SINGLE_DOCCOMMENT, 
+			DeePartitions.DEE_MULTI_DOCCOMMENT, 
+			null);
 		
-		rules.add(new PatternRule_Fixed("///", null, tkSingleDocComment, NO_ESCAPE_CHAR, true, true)); 
-		rules.add(new PatternRule_Fixed("//", null, tkSingleComment, NO_ESCAPE_CHAR, true, true));
+		rules.add(new NestedDelimiterRule("/++", "/+", "+/", new Token(DEE_NESTED_DOCCOMMENT), NO_ESCAPE_CHAR, true));
+		rules.add(new NestedDelimiterRule("/+", "/+", "+/", new Token(DEE_NESTED_COMMENT), NO_ESCAPE_CHAR, true));
 		
-		rules.add(new NestedDelimiterRule("/++", "/+", "+/", tkNestedDocComment, NO_ESCAPE_CHAR, true));
-		rules.add(new NestedDelimiterRule("/+", "/+", "+/", tkNestedComment, NO_ESCAPE_CHAR, true));
-		rules.add(new PatternRule_Fixed("/**", "*/", tkMultiDocComment, NO_ESCAPE_CHAR, false, true));
-		rules.add(new PatternRule_Fixed("/*", "*/", tkMultiComment, NO_ESCAPE_CHAR, false, true));
-		
-		
-		setPredicateRules(rules.toArray(IPredicateRule.class));
 	}
 	
 }
