6b00f93fa53ba7cdb442ddb2d4066a26219ea58c
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2015-06-02 12:46:26 +0100

    LANG: Minor LangDocumentPartitionerSetup refactoring.

--- plugin_ide.core/src-lang/melnorme/lang/ide/core/text/LangDocumentPartitionerSetup.java
+++ plugin_ide.core/src-lang/melnorme/lang/ide/core/text/LangDocumentPartitionerSetup.java
@@ -12,7 +12,6 @@ package melnorme.lang.ide.core.text;
 
 import static melnorme.utilbox.core.Assert.AssertNamespace.assertNotNull;
 import melnorme.lang.ide.core.TextSettings_Actual;
-import melnorme.utilbox.misc.ArrayUtil;
 
 import org.eclipse.core.filebuffers.IDocumentSetupParticipant;
 import org.eclipse.jface.text.IDocument;
@@ -22,9 +21,6 @@ import org.eclipse.jface.text.rules.FastPartitioner;
 import org.eclipse.jface.text.rules.IPartitionTokenScanner;
 
 public class LangDocumentPartitionerSetup implements IDocumentSetupParticipant {
-	
-	public static final String[] LEGAL_CONTENT_TYPES = 
-			ArrayUtil.remove(TextSettings_Actual.PARTITION_TYPES, IDocument.DEFAULT_CONTENT_TYPE);
 	
 	protected static LangDocumentPartitionerSetup instance = new LangDocumentPartitionerSetup();
 	
@@ -37,21 +33,22 @@ public class LangDocumentPartitionerSetup implements IDocumentSetupParticipant {
 	
 	@Override
 	public void setup(IDocument document) {
-		setupDocumentPartitioner(document, TextSettings_Actual.PARTITIONING_ID);
+		setupDocument(document);
 	}
 	
-	public IPartitionTokenScanner createPartitionScanner() {
-		return TextSettings_Actual.createPartitionScanner();
+	public FastPartitioner setupDocument(IDocument document) {
+		return setupDocumentPartitioner(document, TextSettings_Actual.PARTITIONING_ID);
 	}
 	
-	public final IDocumentPartitioner createDocumentPartitioner() {
-		IPartitionTokenScanner scanner = createPartitionScanner();
-		return new FastPartitioner(scanner, LEGAL_CONTENT_TYPES);
+	public FastPartitioner createDocumentPartitioner() {
+		IPartitionTokenScanner scanner = TextSettings_Actual.createPartitionScanner();
+		return new FastPartitioner(scanner, TextSettings_Actual.LEGAL_CONTENT_TYPES);
 	}
 	
-	protected void setupDocumentPartitioner(IDocument document, String partitioning) {
-		IDocumentPartitioner partitioner = createDocumentPartitioner();
+	protected FastPartitioner setupDocumentPartitioner(IDocument document, String partitioning) {
+		FastPartitioner partitioner = createDocumentPartitioner();
 		setupDocumentPartitioner$2(partitioner, document, partitioning);
+		return partitioner;
 	}
 	
 	public static void setupDocumentPartitioner$2(IDocumentPartitioner partitioner, IDocument document, 
