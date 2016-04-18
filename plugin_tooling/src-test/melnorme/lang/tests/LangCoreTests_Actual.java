adb9fc34283f73a3801920fa85ef459475be9073
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2016-04-18 16:47:27 +0100

    addendum to merge

--- plugin_tooling/src-test/melnorme/lang/tests/LangCoreTests_Actual.java
+++ plugin_tooling/src-test/melnorme/lang/tests/LangCoreTests_Actual.java
@@ -16,6 +16,7 @@ import melnorme.utilbox.misc.Location;
 @LANG_SPECIFIC
 public class LangCoreTests_Actual {
 	
-	public static Location SAMPLE_SDK_PATH = LangToolingTestResources.getTestResourceLoc("default_SDK");
+	public static Location SAMPLE_SDK_PATH = LangToolingTestResources.getTestResourceLoc("default_SDK")
+			.resolve_fromValid("bin/dub");
 	
 }
